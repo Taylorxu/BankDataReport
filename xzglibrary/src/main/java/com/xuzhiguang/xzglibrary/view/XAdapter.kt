0b8673cb9f6b35825001f6c24d29d7634a4318eb
Taylorxu <zhiguang_xu@live.cn>
2018-06-28 13:52:25 +0800

    first commit

---
+++ xzglibrary/src/main/java/com/xuzhiguang/xzglibrary/view/XAdapter.kt
@@ -0,0 +1,79 @@
+package com.xuzhiguang.xzglibrary.view
+
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by 徐志广 on 2018/4/11.
+ */
+open abstract class XAdapter<Data, Binding : ViewDataBinding> : RecyclerView.Adapter<XViewHolder<Data, Binding>>() {
+    var dataList = mutableListOf<Data>()
+
+    override fun getItemCount(): Int {
+        return dataList!!.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): XViewHolder<Data, Binding> {
+        var viewHolder: XViewHolder<Data, Binding>? = XViewHolder(LayoutInflater.from(parent?.context).inflate(holderLayout(viewType), parent, false))
+        return viewHolder!!
+    }
+
+    abstract fun holderLayout(viewType: Int): Int
+
+    //variableId 是R.layout.xxx 中<layout><data></data></layout> BR.data
+    open class SimpleAdapter<Data, Binding : ViewDataBinding>(private var variableId: Int, private var holderLayout: Int, private val itemClick: (Data, Binding) -> Unit) : XAdapter<Data, Binding>() {
+
+        //holderLayout 是R.layout.xxx 布局文件在资源中ID
+        override fun holderLayout(viewType: Int): Int {
+            return holderLayout
+        }
+
+        override fun onBindViewHolder(holder: XViewHolder<Data, Binding>?, position: Int) {
+            holder?.fill(variableId, dataList!![position])
+            holder?.itemView?.setOnClickListener { itemClick(dataList!![position], holder?.binding!!) }
+        }
+
+
+    }
+
+    //初始
+    fun setList(l: MutableList<Data>) {
+        dataList = l
+        notifyDataSetChanged()
+    }
+
+    //追加list
+    fun addItems(l: MutableList<Data>) {
+        if (l === null || l.isEmpty()) return
+        dataList?.addAll(l)
+        notifyItemInserted(dataList?.size?.let { it - 1 })
+        notifyItemRangeChanged(dataList?.size - l.size, l.size)
+    }
+
+    //追加item
+    fun addItem(data: Data) {
+        dataList?.add(data)
+        notifyItemInserted(dataList?.size?.let { it - 1 })
+    }
+
+    //指定位置
+    fun addItem(data: Data, position: Int) {
+        dataList.add(position, data)
+        notifyItemInserted(position)
+    }
+
+    //删除指定
+    fun removeItem(position: Int) {
+        dataList.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    //删除全部
+    fun removeAll() {
+        dataList.clear()
+        notifyDataSetChanged()
+    }
+}
---
+++ xzglibrary/src/main/java/com/xuzhiguang/xzglibrary/view/xViewElement/XNavigationBar.kt
@@ -0,0 +1,85 @@
+package com.xuzhiguang.xzglibrary.view.xViewElement
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import com.xuzhiguang.xzglibrary.R
+import com.xuzhiguang.xzglibrary.helperTool.DensityHelper
+import kotlinx.android.synthetic.main.layout_navigation_bar_group.view.*
+import org.jetbrains.anko.forEachChild
+
+/**
+ * 自定义 navigationBar
+ * Created by 徐志广 on 2018/4/24.
+ */
+class XNavigationBar : LinearLayout {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    var onCheckedChangeInterface: OnCheckedChangeListener? = null
+    var p_top_c = DensityHelper.dp2px(6f)  //被选中时，paddingTop
+    var p_top_ = DensityHelper.dp2px(8f)
+    var p_bottom = DensityHelper.dp2px(0f)
+    val text_size_c = 12f  //被选中时，字体大小设置为 12
+    val text_size_ = 11f
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_navigation_bar_group, this, true)
+        rb_0.isChecked = true
+        initViewParam()
+        rb_group.setOnCheckedChangeListener { group, checkedId -> onCheckedChangeListener(checkedId) }
+    }
+
+    private fun initViewParam() {
+        rb_group.forEachChild { view -> setEachChild(view as RadioButton) }
+    }
+
+    fun setEachChild(v: RadioButton) {
+        if (v.isChecked) {
+            v.textSize = text_size_c
+            v.setPadding(0, p_top_c, 0, p_bottom)
+        } else {
+            v.setPadding(0, p_top_, 0, p_bottom)
+            v.textSize = text_size_
+        }
+
+    }
+
+    var oldChecked: RadioButton? = null
+    fun onCheckedChangeListener(checkedId: Int) {
+        when (checkedId) {
+            R.id.rb_0 -> {
+                reSetOldChecked(rb_0)
+            }
+            R.id.rb_1 -> {
+                reSetOldChecked(rb_1)
+            }
+            R.id.rb_2 -> {
+                reSetOldChecked(rb_2)
+            }
+            R.id.rb_3 -> {
+                reSetOldChecked(rb_3)
+            }
+
+        }
+    }
+
+    fun reSetOldChecked(newChecked: RadioButton) {
+        newChecked.setPadding(0, p_top_c, 0, p_bottom)
+        newChecked.textSize = text_size_c
+        oldChecked?.setPadding(0, p_top_, 0, p_bottom)
+        oldChecked?.textSize = text_size_
+        oldChecked = newChecked
+        onCheckedChangeInterface?.onCheckedChanged(newChecked.id)
+    }
+
+    fun setOnCheckedChangeListenter(listener: OnCheckedChangeListener) {
+        if (onCheckedChangeInterface === null) onCheckedChangeInterface = listener
+    }
+
+    interface OnCheckedChangeListener {
+        fun onCheckedChanged(checkedId: Int)
+    }
+
+}
